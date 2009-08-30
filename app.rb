eaab8abcc6000ebaf2218e5241732692a375b4fa
Pablo Brasero <pablobm@gmail.com>
2009-08-30 15:37:04 +0200

    Render loans as markers on a map.

--- app.rb
+++ app.rb
@@ -6,5 +6,6 @@ require 'json'
 get '/' do
   json_data = Net::HTTP.get URI.parse('http://api.kivaws.org/v1/lenders/pablobm/loans.json')
   data = JSON.parse(json_data)
-  loans = data['loans']
+  @loans = data['loans']
+  erb :index
 end
