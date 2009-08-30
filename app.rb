bf417e1f8e2501cdbdd7a02c96ca078ab03d6970
Pablo Brasero <pablobm@gmail.com>
2009-08-30 14:40:29 +0200

    Retrieve loans from Kiva WS.

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'sinatra'
+require 'net/http'
+require 'json'
 
 get '/' do
-  "This is it"
+  json_data = Net::HTTP.get URI.parse('http://api.kivaws.org/v1/lenders/pablobm/loans.json')
+  data = JSON.parse(json_data)
+  loans = data['loans']
 end
