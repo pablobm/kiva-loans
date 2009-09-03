72e51e52cd0d5ef1f4a91ee3e5aad4d081b3d4c4
Pablo Brasero <pablobm@gmail.com>
2009-09-03 17:21:03 +0200

    Flash message on error.

--- lib/kiva_loans_app.rb
+++ lib/kiva_loans_app.rb
@@ -1,8 +1,12 @@
 require 'sinatra/base'
 require 'net/http'
 require 'json'
+require 'lib/sinatra_flash'
 
 class KivaLoansApp < Sinatra::Base
+
+  include SinatraFlash
+
   get '/' do
     user_id = params[:user_id]
     if user_id
@@ -18,6 +22,7 @@ class KivaLoansApp < Sinatra::Base
     if @loans
       erb :map
     else
+      flash[:error] = "User #{user_id} does not exist"
       redirect '/'
     end
   end
---
+++ lib/sinatra_flash.rb
@@ -0,0 +1,24 @@
+# Took the code at http://blog.kabisa.nl/2009/02/16/flash-messages-for-sinatra/
+# and made it into a mixin
+module SinatraFlash
+
+  def self.included(base)
+    base.module_eval do
+      enable :sessions
+      alias_method :erb_without_flash, :erb
+      alias_method :erb, :erb_with_flash
+    end
+  end
+
+  def flash
+    session[:flash] = {} if session[:flash] && session[:flash].class != Hash
+    session[:flash] ||= {}
+  end
+
+  def erb_with_flash(*args)
+    ret = erb_without_flash(*args)
+    flash.clear
+    ret
+  end
+
+end
